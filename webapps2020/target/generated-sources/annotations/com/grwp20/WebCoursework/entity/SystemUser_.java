b8b36b85586eec00646d1d8a091056b68d9edac6
GregPyemont <37701095+GregPyemont@users.noreply.github.com>
2023-01-20 18:53:57 +0000

    Added

--- webapps2020/target/generated-sources/annotations/com/grwp20/WebCoursework/entity/SystemUser_.java
+++ webapps2020/target/generated-sources/annotations/com/grwp20/WebCoursework/entity/SystemUser_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-07T05:32:15")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-01-20T18:19:28")
 @StaticMetamodel(SystemUser.class)
 public class SystemUser_ { 
 
